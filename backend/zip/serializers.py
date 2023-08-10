ecba7b58261852c8f0808a4aa0a84f07c5a2e724
newxxson <hyukjun1111@gmail.com>
2023-08-10 10:54:43 +0900

    updated is_interest error, added is_user to review

--- backend/zip/serializers.py
+++ backend/zip/serializers.py
@@ -3,74 +3,112 @@ from rest_framework.fields import empty
 from .models import Area, House, Review, Keyword
 
 
-
-
-
 class HouseSerializer(serializers.ModelSerializer):
-    area_code = serializers.CharField(source='area.area_code')
-    area_name = serializers.CharField(source='area.area_name')
+    area_code = serializers.CharField(source="area.area_code")
+    area_name = serializers.CharField(source="area.area_name")
     is_interested = serializers.SerializerMethodField()
     rat_avg = serializers.SerializerMethodField()
 
     class Meta:
         model = House
-        fields = ['area_code', 'area_name', 'id', 'address', 'name', 'lat', 'lng',  'review_num', 'rat_avg', 'suggest_ratio', 'rat_avg', 'is_interested', ]
-    
+        fields = [
+            "area_code",
+            "area_name",
+            "id",
+            "address",
+            "name",
+            "lat",
+            "lng",
+            "review_num",
+            "rat_avg",
+            "suggest_ratio",
+            "rat_avg",
+            "is_interested",
+        ]
+
     def get_is_interested(self, house):
-        user = self.context['request'].user
+        user = self.context["request"].user
         return house.interested_users.filter(id=user.id).exists()
+
     def get_rat_avg(self, house):
         reviews = house.reviews.all()
         total = 0
         for review in reviews:
             total += review.rating_overall
-        return total // reviews.count()   
+        return total // reviews.count()
 
 
 class HouseSerializerSimple(serializers.ModelSerializer):
-    area_name = serializers.CharField(source='area.area_name')
-    img_urls = serializers.SerializerMethodField()  
+    area_name = serializers.CharField(source="area.area_name")
+    img_urls = serializers.SerializerMethodField()
     rat_avg = serializers.SerializerMethodField()
+
     class Meta:
         model = House
-        fields = [ 'area_name', 'id','address', 'name', 'lat', 'lng', 'suggest_ratio', 'img_urls', 'rat_avg', 'is_interested']
+        fields = [
+            "area_name",
+            "id",
+            "address",
+            "name",
+            "lat",
+            "lng",
+            "suggest_ratio",
+            "img_urls",
+            "rat_avg",
+            "is_interested",
+        ]
 
     def get_img_urls(self, house):
         # Get all img_url values from linked reviews and return as a list
         reviews = house.reviews.all()
         return [review.img_url for review in reviews]
-    
-    
-        
-    
+
+    def get_is_interested(self, house):
+        user = self.context["request"].user
+        return house.interested_users.filter(id=user.id).exists()
+
+
 class AreaSerializerSimple(serializers.ModelSerializer):
     class Meta:
         model = Area
-        fields = ['area_code', 'area_name']
+        fields = ["area_code", "area_name"]
+
 
 class AreaSerializer(serializers.ModelSerializer):
     house = serializers.SerializerMethodField()
+
     class Meta:
         model = Area
-        fields = ['area_code', 'area_name', 'house']
-    
+        fields = ["area_code", "area_name", "house"]
+
     def get_house(self, area):
         houses = area.houses.all()
-        return [{'name': house.name, 'pk' : house.pk} for house in houses]
+        return [{"name": house.name, "pk": house.pk} for house in houses]
+
 
 class ReviewSerializer(serializers.ModelSerializer):
     selected_keywords = serializers.SerializerMethodField()
+    is_user = serializers.SerializerMethodField()
+
     class Meta:
         model = Review
-        exclude = ['user', 'keywords', 'house']
-    
+        exclude = ["user", "keywords", "house"]
+
     def get_selected_keywords(self, review):
         keywords = review.keywords.all()
-        return [{'description':keyword.description, 'icon_url':keyword.icon_url} for keyword in keywords]
+        return [
+            {"description": keyword.description, "icon_url": keyword.icon_url}
+            for keyword in keywords
+        ]
+
+    def get_is_user(self, review):
+        if self.context["user"] == review.user:
+            return True
+        else:
+            return False
+
 
 class KeywordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Keyword
-        fields = ['key_type', 'description', 'icon_url', 'pk']
-        
-    
+        fields = ["key_type", "description", "icon_url", "pk"]
--- backend/zip/views.py
+++ backend/zip/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from .serializers import HouseSerializer, HouseSerializerSimple, AreaSerializer, AreaSerializerSimple, KeywordSerializer, ReviewSerializer
+from .serializers import (
+    HouseSerializer,
+    HouseSerializerSimple,
+    AreaSerializer,
+    AreaSerializerSimple,
+    KeywordSerializer,
+    ReviewSerializer,
+)
 from rest_framework import viewsets
 from .models import Area, House, Keyword, Review
 from rest_framework.response import Response
@@ -12,42 +19,45 @@ from rest_framework.decorators import api_view, permission_classes
 import json
 from backend.settings import NAVER_ID, NAVER_SECRET
 from .utils import slice_and_get_coordinates, check_query
+
 # Create your views here.
 
 
-#used to see houses related to each area, can specify area
+# used to see houses related to each area, can specify area
 class AreaView(viewsets.ModelViewSet):
     serializer_class = AreaSerializer
     queryset = Area.objects.all()
     permission_classes = [IsAuthenticated, IsReadOnlyOrAdmin]
-    lookup_field = 'area_code'
-    
+    lookup_field = "area_code"
+
     def list(self, request, *args, **kwargs):
         query_params = request.query_params
 
         if not query_params:
             return super().list(request, *args, **kwargs)
-        elif query_params.get('simple'):
+        elif query_params.get("simple"):
             serializer = AreaSerializerSimple(instance=self.queryset, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response({'message': 'Invalid query parameter'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        
+            return Response(
+                {"message": "Invalid query parameter"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
-#1. retreive only coordinates and ratio
-#2. retreive all the house instance => pagination not supported yet 
-#3. retreive specific house instance with address(postal_code)
-#4. retreive house matching to search parameter
+
+# 1. retreive only coordinates and ratio
+# 2. retreive all the house instance => pagination not supported yet
+# 3. retreive specific house instance with address(postal_code)
+# 4. retreive house matching to search parameter
 class HouseView(viewsets.ModelViewSet):
     serializer_class = HouseSerializer
-    lookup_field = 'address'
+    lookup_field = "address"
     queryset = House.objects.all()
     permission_classes = [IsAuthenticated, IsReadOnlyOrAdmin]
 
-    #simple list는 허용됨
+    # simple list는 허용됨
     def get_permissions(self):
-        if self.action == 'list' and 'simple' in self.request.query_params:
+        if self.action == "list" and "simple" in self.request.query_params:
             return []
         return super().get_permissions()
 
@@ -55,38 +65,38 @@ class HouseView(viewsets.ModelViewSet):
         query_params = request.query_params
         instance = self.queryset
         if not query_params:
-            print('no query')
+            print("no query")
             serializer = self.get_serializer(instance, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
-        elif 'simple' in query_params:
-            print('simple')
+        elif "simple" in query_params:
+            print("simple")
             serializer = self.get_serializer(instance, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            raise ValidationError('Invalid query parameter.')
-    
-    #주소를 물으면 집들을 리턴, pk를 특정하면 리뷰를 리턴
+            raise ValidationError("Invalid query parameter.")
+
+    # 주소를 물으면 집들을 리턴, pk를 특정하면 리뷰를 리턴
     def retrieve(self, request, *args, **kwargs):
         data = self.kwargs.get(self.lookup_field)
         if data.isnumeric():
-            house = self.queryset.get(pk = data)
+            house = self.queryset.get(pk=data)
             reviews = house.reviews.all()
-            serializer = ReviewSerializer(reviews, many=True)
+            serializer = ReviewSerializer(
+                reviews, many=True, context={"user": request.user}
+            )
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            result = self.queryset.filter(address__contains = data)
+            result = self.queryset.filter(address__contains=data)
             serializer = self.get_serializer(result, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
 
-        
     def get_serializer_class(self):
-        if self.action == 'list' and 'simple' in self.request.query_params:
+        if self.action == "list" and "simple" in self.request.query_params:
             return HouseSerializerSimple
         return self.serializer_class
-    
-    
-  
 
+
+# deprececated now search is used from multi search view but create is still used
 class ReviewView(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     queryset = Review.objects.all()
@@ -96,9 +106,11 @@ class ReviewView(viewsets.ModelViewSet):
         query_params = request.query_params
 
         if not query_params:
-            serializer = self.get_serializer(self.queryset, many=True)
+            serializer = self.get_serializer(
+                self.queryset, many=True, context={"user": request.user}
+            )
             return Response(serializer.data, status=status.HTTP_200_OK)
-        
+
         query_list = dict()
 
         for query in query_params:
@@ -108,203 +120,216 @@ class ReviewView(viewsets.ModelViewSet):
         serializer = self.get_serializer(result, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    #용어를 정리할 필요가 있음
+    # 용어를 정리할 필요가 있음
     def check_query(self, query, query_params, query_list):
         condition = query_params.get(query)
         print(query, type(query), condition, type(condition))
-        if query == 'area':
-            query_list['house__area__area_code'] = condition
-        elif query == 'rent':
-            if condition == 'jeon':
-                query_list['rent_type'] = condition
-            elif condition == 'monthly':
-                query_list['rent_type'] = condition
-        elif query == 'dlb':
-            query_list['deposit__gte'] = int(condition)
-        elif query == 'dub':
-            query_list['deposit__lte'] = int(condition)
-        elif query == 'mlb':
-            query_list['monthly__gte'] = int(condition)
-        elif query == 'mub':
-            query_list['monthly__lte'] = int(condition)
+        if query == "area":
+            query_list["house__area__area_code"] = condition
+        elif query == "rent":
+            if condition == "jeon":
+                query_list["rent_type"] = condition
+            elif condition == "monthly":
+                query_list["rent_type"] = condition
+        elif query == "dlb":
+            query_list["deposit__gte"] = int(condition)
+        elif query == "dub":
+            query_list["deposit__lte"] = int(condition)
+        elif query == "mlb":
+            query_list["monthly__gte"] = int(condition)
+        elif query == "mub":
+            query_list["monthly__lte"] = int(condition)
 
     def create(self, request, *args, **kwargs):
         print(request.data)
         try:
-            data_dict = request.data.get('json_data')
+            data_dict = request.data.get("json_data")
             data_dict = json.loads(data_dict)
         except:
             data_dict = request.data
             print(type(data_dict))
-        
-        address = data_dict.pop('address')
+
+        address = data_dict.pop("address")
         address, lat, lng = slice_and_get_coordinates(address)
-        house_queryset = House.objects.filter(address = address)
+        house_queryset = House.objects.filter(address=address)
         try:
             if house_queryset.exists():
                 house_instance = house_queryset.first()
 
                 if house_instance.reviews.filter(user=request.user).exists():
-                    return Response({'message' : 'the user has already written the Review'}, status=status.HTTP_409_CONFLICT)
+                    return Response(
+                        {"message": "the user has already written the Review"},
+                        status=status.HTTP_409_CONFLICT,
+                    )
 
-                #house 정보 수정
+                # house 정보 수정
                 suggest_ratio = house_instance.suggest_ratio
                 review_num = house_instance.review_num
-                if data_dict.get('suggest') == 1:
-                    print('suggesting')
-                    house_instance.suggest_ratio = "%.2f" % ((review_num * suggest_ratio + 1) / (review_num + 1))
+                if data_dict.get("suggest") == 1:
+                    print("suggesting")
+                    house_instance.suggest_ratio = "%.2f" % (
+                        (review_num * suggest_ratio + 1) / (review_num + 1)
+                    )
                 else:
-                    print('nosuggest')
-                    house_instance.suggest_ratio = "%.2f" % ((review_num * suggest_ratio) / (review_num + 1))
+                    print("nosuggest")
+                    house_instance.suggest_ratio = "%.2f" % (
+                        (review_num * suggest_ratio) / (review_num + 1)
+                    )
                 house_instance.review_num += 1
                 house_instance.save()
             else:
-                if data_dict.get('suggest') == 1:
+                if data_dict.get("suggest") == 1:
                     suggest_ratio = 1
                 else:
                     suggest_ratio = 0
-                area = Area.objects.get(area_code = data_dict.get('area'))
+                area = Area.objects.get(area_code=data_dict.get("area"))
                 house_instance = House.objects.create(
-                    area = area,
-                    address = address,
-                    lat = lat,
-                    lng = lng,
-                    name = data_dict.get('name'),
-                    suggest_ratio = suggest_ratio
+                    area=area,
+                    address=address,
+                    lat=lat,
+                    lng=lng,
+                    name=data_dict.get("name"),
+                    suggest_ratio=suggest_ratio,
                 )
-                print('house_created')
-            
-            #review 형식에 맞게 수정
-            data_dict.pop('area')
-            data_dict.pop('name')
-            print('???')
-            key_pk = data_dict.pop('keywords')
+                print("house_created")
+
+            # review 형식에 맞게 수정
+            data_dict.pop("area")
+            data_dict.pop("name")
+            key_pk = data_dict.pop("keywords")
             print(key_pk)
             keywords = Keyword.objects.filter(pk__in=key_pk)
 
-            print('herhere')
-            img_url = ''
+            print("herhere")
+            img_url = ""
             try:
-                image_data = request.FILES.get('image_data')
+                image_data = request.FILES.get("image_data")
             except Exception as e:
-                img_url = 'https://test.com/testtest.png'
+                img_url = "https://test.com/testtest.png"
             if image_data:
-                print('there is an image file')
-                img_url = 'https://test.com/testtesttest.png'
+                print("there is an image file")
+                img_url = "https://test.com/testtesttest.png"
             review_instance = Review.objects.create(
-                user = request.user,
-                house = house_instance,
-                img_url = img_url,
-                **data_dict
+                user=request.user, house=house_instance, img_url=img_url, **data_dict
             )
             review_instance.keywords.set(keywords)
             review_instance.save()
             serializer = self.get_serializer(review_instance)
 
-
-
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
 
         except Exception as e:
-            return Response({'message' : e}, status=status.HTTP_400_BAD_REQUEST)
-
-
+            return Response({"message": e}, status=status.HTTP_400_BAD_REQUEST)
 
     def partial_update(self, request, *args, **kwargs):
         instance = self.get_object()
         house = instance.house
         data_dict = request.data
 
-        suggest = data_dict.get('suggest')
+        suggest = data_dict.get("suggest")
 
-        #집 suggest_ratio 수정 
+        # 집 suggest_ratio 수정
         if suggest != None:
-            if suggest == 1 and  instance.suggest == 0: 
-                house.suggest_ratio = "%.2f" % ((house.suggest_ratio * house.review_num + 1) / house.review_num)
+            if suggest == 1 and instance.suggest == 0:
+                house.suggest_ratio = "%.2f" % (
+                    (house.suggest_ratio * house.review_num + 1) / house.review_num
+                )
             elif suggest == 0 and instance.suggest == 1:
-                house.suggest_ratio = "%.2f" % ((house.suggest_ratio * house.review_num - 1) / house.review_num)
+                house.suggest_ratio = "%.2f" % (
+                    (house.suggest_ratio * house.review_num - 1) / house.review_num
+                )
             house.save()
         return super().partial_update(request, *args, **kwargs)
-    
+
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         house = instance.house
         if house.review_num == 1:
-            print('destroy house')
+            print("destroy house")
             return super().destroy(request, *args, **kwargs)
-        
+
         if instance.suggest:
-            house.suggest_ratio = "%.2f" % ((house.suggest_ratio * house.review_num - 1) / (house.review_num - 1))
+            house.suggest_ratio = "%.2f" % (
+                (house.suggest_ratio * house.review_num - 1) / (house.review_num - 1)
+            )
         else:
-            house.suggest_ratio = "%.2f" % ((house.suggest_ratio * house.review_num) / (house.review_num - 1))        
-        house.review_num -= 1        
+            house.suggest_ratio = "%.2f" % (
+                (house.suggest_ratio * house.review_num) / (house.review_num - 1)
+            )
+        house.review_num -= 1
         house.save()
         return super().destroy(request, *args, **kwargs)
-    
+
 
 class KeywordView(viewsets.ModelViewSet):
     permission_classes = [IsReadOnlyOrAdmin]
     serializer_class = KeywordSerializer
-    queryset = Keyword.objects.all().order_by('key_type')
+    queryset = Keyword.objects.all().order_by("key_type")
 
     def list(self, request, *args, **kwargs):
         key_choices = [
-        ('INFRA', '내부'),
-        ('ROOM', '룸컨디션'),
-        ('SAFETY', '치안'),
-        ('TRANSPORT', '교통')
+            ("INFRA", "내부"),
+            ("ROOM", "룸컨디션"),
+            ("SAFETY", "치안"),
+            ("TRANSPORT", "교통"),
         ]
         queryset = self.get_queryset()
         grouped_data = {}
-        for key_type, group in groupby(queryset, key=lambda obj: dict(key_choices).get(obj.key_type)):
+        for key_type, group in groupby(
+            queryset, key=lambda obj: dict(key_choices).get(obj.key_type)
+        ):
             grouped_data[key_type] = [
-                {
-                    'description' : obj.description, 
-                    'icon_url' : obj.icon_url, 
-                    'pk' : obj.pk
-                }
-                for obj in group]
+                {"description": obj.description, "icon_url": obj.icon_url, "pk": obj.pk}
+                for obj in group
+            ]
 
         return Response(grouped_data, status=status.HTTP_200_OK)
-    
 
 
 @permission_classes([IsAuthenticated])
-@api_view(['GET'])
+@api_view(["GET"])
 def address_area_multi_search(request, search):
     query_params = request.GET.copy()
-    if search == 'area':
-        search_areas = query_params.pop('area')
+    if search == "area":
+        search_areas = query_params.pop("area")
         print(search_areas)
-        houses = House.objects.filter(area__area_code__in = search_areas)
-        print('area_house', houses.count())
+        houses = House.objects.filter(area__area_code__in=search_areas)
+        print("area_house", houses.count())
         if not houses.exists():
-            return Response({'message':'no search result'}, status=status.HTTP_404_NOT_FOUND)  
-    elif search == 'address':
-        address = query_params.pop('address')[0]
-        houses = House.objects.filter(address__contains = address)
-        print('address', address)
+            return Response(
+                {"message": "no search result"}, status=status.HTTP_404_NOT_FOUND
+            )
+    elif search == "address":
+        address = query_params.pop("address")[0]
+        houses = House.objects.filter(address__contains=address)
+        print("address", address)
         if not houses.exists():
-            return Response({'message':'no search result'}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {"message": "no search result"}, status=status.HTTP_404_NOT_FOUND
+            )
     else:
-        return Response({'message': 'invalid search type'}, status=status.HTTP_400_BAD_REQUEST)
-    
+        return Response(
+            {"message": "invalid search type"}, status=status.HTTP_400_BAD_REQUEST
+        )
+
     query_list = dict()
     for query in query_params:
         check_query(query, query_params, query_list)
     if query_list:
-        print('query existed')
+        print("query existed")
         reviews = Review.objects.filter(**query_list)
-        houses = houses.filter(reviews__in = reviews).distinct()
-    
-    serializer = HouseSerializer(instance=houses, many=True, context={'request':request})
+        houses = houses.filter(reviews__in=reviews).distinct()
+
+    serializer = HouseSerializer(
+        instance=houses, many=True, context={"request": request}
+    )
     data = serializer.data
 
-    #이거 그냥 다 주면 좋을 것 같은데
-    if search == 'area':
-        data = {area_code: list(house) for area_code, house in groupby(data, key=lambda x: x['area_code'])}
+    # 이거 그냥 다 주면 좋을 것 같은데
+    if search == "area":
+        data = {
+            area_code: list(house)
+            for area_code, house in groupby(data, key=lambda x: x["area_code"])
+        }
 
     return Response(data, status=status.HTTP_200_OK)
-
-    
