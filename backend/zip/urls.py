ca0db35501a3d0fa50b30722eaf3883252576777
newxxson <hyukjun1111@gmail.com>
2023-08-11 16:05:14 +0900

    cleared

--- backend/zip/urls.py
+++
@@ -1,8 +0,0 @@
-# # zip/urls.py
-# from django.urls import include, path
-# from rest_auth.views import LoginView
-# from .serializers import CustomLoginSerializer
-# from .admin import CustomUserAdmin
-# urlpatterns = [
-    
-# ]
