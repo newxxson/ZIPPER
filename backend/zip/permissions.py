07a6cf229251ec1c052993c6aa343fcb2bf565fa
newxxson <hyukjun1111@gmail.com>
2023-08-13 01:21:17 +0900

    changed review permission

--- backend/zip/permissions.py
+++ backend/zip/permissions.py
@@ -1,20 +1,25 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
+
 class IsWriterOrAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in ['PATCH', 'PUT', 'DELETE']:
+        if request.method in ["PATCH", "PUT", "DELETE"]:
             # Allow update (PATCH, PUT, DELETE) only for the writer of the object or an admin
             return obj.user == request.user or request.user.is_staff
         else:
             # Allow other methods (GET, POST, DELETE) for all users
             return True
-        
+
 
 class IsReadOnlyOrAdmin(BasePermission):
     def has_permission(self, request, view):
+        # Allow read-only methods for all users
         if request.method in SAFE_METHODS:
-            # Allow read-only (GET, HEAD, OPTIONS) for all users
             return True
-        else:
-            # Allow other methods (POST, PUT, PATCH, DELETE) only for admin users
-            return request.user and request.user.is_staff
+
+        # Allow POST for all authenticated users
+        if request.method == "POST" and request.user.is_authenticated:
+            return True
+
+        # Allow other non-safe methods only for admin users
+        return request.user and request.user.is_staff
