b959e33848854c0bdd801988e1e0dbb5c0b872d0
newxxson <hyukjun1111@gmail.com>
2023-08-31 14:26:04 +0900

    added reviewimage to admin

--- backend/zip/admin.py
+++ backend/zip/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import House, Area, Review, Keyword
+from .models import House, Area, Review, Keyword, ReviewImage
+
 # Register your models here.
+
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
     list = [field.name for field in House._meta.get_fields()]
-
 
 
 @admin.register(Area)
@@ -20,6 +21,12 @@ class AreaAdmin(admin.ModelAdmin):
 class ReviewAdmin(admin.ModelAdmin):
     list = [field.name for field in Review._meta.get_fields()]
 
+
 @admin.register(Keyword)
 class KeywordAdmin(admin.ModelAdmin):
     list = [field.name for field in Keyword._meta.get_fields()]
+
+
+@admin.register(ReviewImage)
+class ReviewImageAdmin(admin.ModelAdmin):
+    list = [field.name for field in ReviewImage._meta.get_fields()]
